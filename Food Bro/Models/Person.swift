96933039d51c9e3afbbc8331120e4867c287b0c8
Tomasz Parys <tomasz.parys@montrosesoftware.com>
2024-06-28 14:03:13 +0200

    code refactoring + created Person Model + Notification.Name+Ext

---
+++ Food Bro/Models/Person.swift
@@ -0,0 +1,26 @@
+//
+//  Person.swift
+//  Food Bro
+//
+//  Created by Tomasz Parys on 27/06/2024.
+//
+
+import Foundation
+
+struct Person {
+    let age: Int
+    let weight: Double
+    let height: Double
+    let bmi: Double
+    let gender: Gender
+    var foodPreferences: String
+    var foodAllergies: String
+    var sportGoals: String
+    var activityLevel: ActivityLevel
+    var dailyCalories: Double
+}
+
+enum Gender: Int, CaseIterable  {
+    case male = 0
+    case female = 1
+}
---
+++ Food Bro/Utils/Extensions/NotificationName+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName+Ext.swift
+//  Food Bro
+//
+//  Created by Tomasz Parys on 28/06/2024.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let startAgain = Notification.Name("startAgain")
+}
